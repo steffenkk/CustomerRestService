a133eeb644c8648ee21e8238949ab8647df4c5c8
steffen klempau <steffen.klempau@yahoo.de>
2020-06-24 12:41:40 +0200

    [dev] add prepared statement

--- src/main/java/com/hausarbeit/DataConnection.java
+++ src/main/java/com/hausarbeit/DataConnection.java
@@ -12,12 +12,12 @@ public class DataConnection {
 
     public void queryTransactions() throws SQLException {
         // query database for the transactions and call setTransactions for the specific customer
-        CachedRowSet result = executeQuery(String.format("SELECT t1.customerid, t1.orderid as orderid, t1.orderdate as date, " +
+        CachedRowSet result = executeQuery("SELECT t1.customerid, t1.orderid as orderid, t1.orderdate as date, " +
                 "t2.category as category, t2.subcategory as subcategory, " +
                 "t2.amount as revenue, t2.quantity as qty, t2.profit as profit FROM orders_list t1 " +
                 "INNER JOIN order_details t2 " +
                 "ON t1.orderid = t2.orderid " +
-                "WHERE t1.customerid = '%s';", customer.getId()));  // the %s stands for String param
+                "WHERE t1.customerid = ?;", customer.getId());  // the %s stands for String param
         while (result.next()) {
             String orderid = result.getString("orderid");
             String date = result.getString("date");
@@ -33,8 +33,8 @@ public class DataConnection {
     public void querySegment() throws SQLException{
         // query database for the segment and return for the specific customer
         String segment = null;
-        CachedRowSet result = executeQuery(String.format("SELECT segment FROM customer WHERE id = '%s';",
-                customer.getId()));
+        CachedRowSet result = executeQuery("SELECT segment FROM customer WHERE CAST(id as VARCHAR(5)) = ?;",
+                customer.getId());
         while (result.next()) {
             segment = result.getString("segment");
         }
@@ -43,16 +43,16 @@ public class DataConnection {
 
     public void queryTransactionsPerCategory() throws SQLException{
         // query db for Transactions per Category
-        CachedRowSet result = executeQuery(String.format("select category, count(DISTINCT t1.orderid) as numbOrders from " +
+        CachedRowSet result = executeQuery("select category, count(DISTINCT t1.orderid) as numbOrders from " +
                         "order_details t1 inner join orders_list t2 ON t1.orderid = t2.orderid where t2.customerid = " +
-                        "'%s' group by 1;", customer.getId()));
+                        "? group by 1;", customer.getId());
         while (result.next()) {
             customer.setTransactionsPerCategory(result.getString("category"),
                     result.getInt("numborders"));
         }
     }
 
-    private CachedRowSet executeQuery(String sqlQuery) {
+    private CachedRowSet executeQuery(String sqlQuery, String id) {
         Connection con = null;
         CachedRowSet crset = null;
         // TODO: ADD ENV DB_PW
@@ -60,8 +60,10 @@ public class DataConnection {
         try {
             con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test1",
                     "postgres", password);
-            Statement stmt = con.createStatement(); // statement object for simple queries
-            ResultSet rs = stmt.executeQuery(sqlQuery);
+            PreparedStatement prepStmt = con.prepareStatement(sqlQuery); // prepStatement object for prepared queries
+            prepStmt.setString(1, (String) id); // update with the id string (replaces a ?)
+            System.out.println(prepStmt.toString());
+            ResultSet rs = prepStmt.executeQuery();
             crset = RowSetProvider.newFactory().createCachedRowSet();
             crset.populate(rs); // store data in the cached-rs to scroll through it without an conn
         } catch (SQLException e) {
